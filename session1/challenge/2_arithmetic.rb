03c8578894663966fb681b6ee34c06b03f807098
Marcus Rands <mrands@hotmail.co.uk>
2017-08-07 10:28:51 +0000

    Updated 2_arithmetic.rb with method. tested and completed

--- session1/challenge/2_arithmetic.rb
+++ session1/challenge/2_arithmetic.rb
@@ -9,4 +9,12 @@
 # arithmetic2(-6, -7)  # => -3.5
 
 def arithmetic2(a, b)
+   a < b ? a.to_f / 2 : b.to_f / 2 
 end
+
+=begin
+1. establish whether A is smaller than b. (a < b)
+2. if a < b, return a / 2
+3. else, return b /2
+4. Make sure answers are floats (.to_f)
+=end
