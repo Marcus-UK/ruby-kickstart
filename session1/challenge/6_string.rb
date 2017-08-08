d8ac5cf044b652f2c095d12ad0b7f9b575987fcd
Marcus Rands <mrands@hotmail.co.uk>
2017-08-08 21:33:30 +0000

    Added completed 6_string.rb

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -8,4 +8,20 @@
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+ to_return = ""
+  string.size.times do |index|
+    next if return_odds && index.even?
+    next if !return_odds && index.odd?
+    to_return << string[index]
+  end
+  to_returnra
 end
+
+=begin
+1. Set up empty string to return results to (to_return = "")
+2. Divide string so that each character can be assessed individually (string.size.times).
+3. Iterate over results, so that ony odd characters are returned when return_odds == true.
+4. Use && Boolean. (return_odds && index.even?)
+5. Append output to variable to be returned (to_return << string[index])
+6. Return variable.
+=end
