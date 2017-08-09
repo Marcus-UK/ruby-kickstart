56f98debc5bbf9ed2d6f6a85618c48b34ede6238
Marcus Rands <mrands@hotmail.co.uk>
2017-08-09 11:35:07 +0000

    updated 1_input_output.rb

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -8,5 +8,8 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
+    num1, num2 = gets.split.map {|num| num.to_i}
+    puts num1 + num2
+    puts num1 - num2
+    puts num1 * num2
 end
