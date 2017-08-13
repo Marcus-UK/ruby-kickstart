b6ca04b91dddaeb0d9b696941d27e047f63d1c15
Marcus Rands <mrands@hotmail.co.uk>
2017-08-13 18:20:30 +0000

    Completed Challenge 2:13

--- session2/challenge/13_classes_iterating_sorting.rb
+++ session2/challenge/13_classes_iterating_sorting.rb
@@ -64,3 +64,54 @@
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
 require 'date'
+
+
+class User
+  attr_accessor :username, :blogs
+
+  def initialize(username)
+    self.username = username
+    self.blogs    = []
+  end
+
+  def add_blog(date, text)
+    added_blog = Blog.new(date, self, text)
+    blogs << added_blog
+    self.blogs = blogs.sort_by { |blog| blog.date }.reverse
+    added_blog
+  end
+end
+
+
+
+class Blog
+  attr_accessor :date, :user, :text
+
+  def initialize(date, user, text)
+    self.date = date
+    self.user = user
+    self.text = text
+  end
+
+  def summary
+    text.split[0..9].join(' ')
+  end
+
+  def entry
+    "#{user.username} #{date}\n#{text}"
+  end
+
+  def ==(other)
+    date   == other.date &&
+      user == other.user &&
+      text == other.text
+  end
+end
+
+
+=begin
+1. Creae class called User.
+2. initializ method with @username.
+3. Create a method add_blog which adds a date and text.
+3. Create a method called blogs which returns an array of all blogs in reverse chrononlogical order.
+=end
