e248c2f07bca960f67b07bd812d1b148099f8e11
Marcus Rands <mrands@hotmail.co.uk>
2017-08-14 14:08:28 +0000

    completed exercise 3:6

--- session3/challenge/6_arguments.rb
+++ session3/challenge/6_arguments.rb
@@ -1,5 +1,5 @@
 # Write a method, match_maker, which will receive an unknown number of
-# elements, and return an array where every two elements are represented by true or false.
+# elements,  and return an array where every two elements are represented by true or false.
 #
 # The very first parameter will not be part of this set. Instead, it will tell
 # you how to determine what their value should be.
@@ -16,4 +16,19 @@
 # match_maker true,  true,  true, false, nil    # => [false, false]
 # match_maker true,  true,  true, true, nil     # => [false, true]
 # match_maker true,  true,  true, 0, nil        # => [false, true]
+
+def match_maker(opposites, *elements)
+    returned = []
+    elements.each_slice 2 do |first, last|
+        first = !!first
+        last  = !!last
+        results = if opposites
+                    first != last
+                  else
+                    first == last
+                  end
+        returned << results
+    end
+    returned
+end
 
